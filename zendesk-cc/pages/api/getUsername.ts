336426c9632210478200392141cb75ff7f65fb57
Oversparkling <nicholasongck@gmail.com>
2021-11-25 14:28:41 +0800

    BE: Support for username

---
+++ zendesk-cc/pages/api/getUsername.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosResponse } from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function ZendeskAPI(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const zendeskSubDomain = "https://zccoversparkling.zendesk.com/";
+  const response = await axios
+    .get(
+      zendeskSubDomain +
+        "api/v2/users/" +
+        req.body.userid,
+      {
+        auth: {
+          username: req.body.username,
+          password: req.body.password,
+        },
+      }
+    )
+    .then((response1) => res.status(200).json(response1.data))
+    .catch((err) => {
+        console.log(err)
+      res.status(401).json(err);
+    });
+}
